ff618e6491c2d32fb4a5b9699c2bdd9a4270446d
PrikolMen <olegis019@gmail.com>
2023-01-21 23:41:17 +0400

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,81 @@
+local convar = convar
+local SERVER = SERVER
+local CLIENT = CLIENT
+local timer = timer
+local Color = Color
+local pcall = pcall
+
+plib = {}
+plib.Chip = chip()
+plib.Owner = owner()
+plib.OwnerIndex = plib.Owner:entIndex()
+
+if (CLIENT) then
+
+    plib.Player = player()
+    plib.Color = Color( 255, 193, 7 )
+    plib.PlayerIsOwner = plib.Player == plib.Owner
+
+    local http = http
+    local bass = bass
+
+    function plib.PlayURL( url, flags, callback )
+        pcall(bass.loadURL, url, flags, function( channel )
+            if isValid( channel ) and isfunction( callback ) then
+                callback( channel )
+            end
+        end)
+    end
+
+    function plib.PlayTTS( text, flags, callback )
+        plib.PlayURL( string.format( "http://translate.google.com/translate_tts?tl=%s&ie=UTF-8&q=%s&client=tw-ob", convar.getString( "gmod_language" ), http.urlEncode( text ) ), flags or '', callback )
+    end
+
+end
+
+if (SERVER) then
+
+    plib.Color = Color( 40, 192, 252 )
+
+    local prop = prop
+
+    do
+        local NULL = NULL
+        function plib.CreateEntity( class, pos, ang, frozen, extraData )
+            local ok, ent = pcall( prop.createSent, pos or plib.Chip:getPos(), ang or plib.Chip:getAngles(), class, frozen or false, extraData )
+            return ok and ent or NULL
+        end
+    end
+
+    function plib.TeleportOwner()
+        local ent = plib.CreateEntity( 'Seat_Airboat', pos, ang, true, {['PLib - Teleporter'] = true} )
+        if isValid( ent ) then
+            ent:setNoDraw( true )
+            ent:use()
+
+            timer.simple(0, function()
+                if isValid( ent ) then
+                    seat:ejectDriver()
+                    seat:remove()
+                end
+            end)
+
+            return ent
+        end
+    end
+
+end
+
+do
+
+    local team = team
+
+    function plib.GetPlayerTeamColor( ply )
+        return team.getColor( ply:getTeam() )
+    end
+
+end
+
+function plib.GetPlayerCenterPos( ply )
+    return ply:localToWorld( ply:obbCenter() )
+end
