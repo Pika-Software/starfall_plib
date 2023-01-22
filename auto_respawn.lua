ad8a5bb4075dcc8cea80b0f0e064b8235fd46ab0
PrikolMen <olegis019@gmail.com>
2023-01-22 04:03:33 +0400

    Create auto_respawn.lua

---
+++ auto_respawn.lua
@@ -0,0 +1,38 @@
+--@name Auto Respawn
+--@author PrikolMen:-b
+--@includedir starfall_plib
+--@server
+
+--[[-----------------
+    Configuration
+-----------------]]--
+
+-- Respawn Delay in Seconds
+local RESPAWN_DELAY = 3
+
+--[[-----------------
+         Code
+-----------------]]--
+dofile( 'starfall_plib/init.lua' )
+local chipName = 'PLib - Auto Respawn'
+
+hook.add('PlayerDeath', chipName, function( ply )
+    if (ply:entIndex() == plib.OwnerIndex) then
+        timer.simple(RESPAWN_DELAY, function()
+            if !isValid( ply ) then return end
+            concmd( '+jump' )
+            timer.simple(0, function()
+                if !isValid( ply ) then return end
+                concmd( '-jump' )
+
+                timer.simple(0.5, function()
+                    if isValid( ply ) and ply:isAlive() then
+                        plib.Log( chipName, 'User has successfully resurrected!' )
+                    else
+                        plib.Log( chipName, 'User revival was a failure...' )
+                    end
+                end)
+            end)
+        end)
+    end
+end)
