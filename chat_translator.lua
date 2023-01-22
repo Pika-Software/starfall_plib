e722000afb95078768f276e5a41346d32acc591d
PrikolMen <olegis019@gmail.com>
2023-01-22 07:44:32 +0400

    Update chat_translator.lua

--- chat_translator.lua
+++ chat_translator.lua
@@ -3,19 +3,83 @@
 --@includedir starfall_plib
 --@shared
 
-local chipName = 'PLib - Chat Translator'
+--[[-----------------
+    Configuration
+-----------------]]--
+
+-- Translate messages from other players
+local TRANSLATE_OTHER_PLAYERS_MESSAGES = true
+local TRANSLATE_FRIENDS_MESSAGES = false
+
+-- Translate your chat messages
+local TRANSLATE_YOUR_MESSAGES = false
+
+-- The language in which your messages will be translated. (https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)
+local TRANSLATE_LANGUAGE = 'pl'
 
 --[[-----------------
          Code
 -----------------]]--
 dofile( 'starfall_plib/init.lua' )
+local chipName = 'PLib - Chat Translator'
+local string = string
+local plib = plib
+local hook = hook
+local http = http
+local net = net
 
--- http://translate.google.ru/translate_a/t?client=x&text={textToTranslate}&hl=en&sl=en&tl=ru
+if CLIENT and plib.PlayerIsOwner then
 
-if (CLIENT) then
+    hook.add('PlayerChat', chipName, function( ply, text )
+        if !TRANSLATE_OTHER_PLAYERS_MESSAGES then return end
+        if plib.IsOwner( ply ) then return end
+        if !TRANSLATE_FRIENDS_MESSAGES and (ply:getFriendStatus() == 'friend') then return end
+        local playerNick, playerColor = ply:getName(), plib.GetPlayerTeamColor( ply )
+        plib.TranslateText( text, plib.GetLanguage(), function( ok, result, languageCode )
+            if (languageCode == plib.GetLanguage()) then return end
+            print( playerColor, playerNick, plib.White, ': ' .. result )
+        end)
+    end)
 
-    hook.add('PlayerChat', chipName, function( ply, text, isTeam, isDead )
+    local messagesCache = {}
+    net.receive(chipName, function()
+        local text, isTeam = net.readString(), net.readBool()
+        local textLower = string.lower( text )
 
+        local cache = messagesCache[ textLower ]
+        if (cache) then
+            plib.Say( cache, isTeam )
+            return
+        end
+
+        plib.TranslateText(text, TRANSLATE_LANGUAGE, function( ok, result )
+            local fullText = http.base64Encode( chipName .. ';' .. result )
+            messagesCache[ textLower ] = fullText
+            plib.Say( fullText, isTeam )
+        end)
+    end)
+
+end
+
+if (SERVER) then
+
+    hook.add('OnPlayerSay', chipName, function( ply, text, isTeam )
+        if !TRANSLATE_YOUR_MESSAGES then return end
+        if plib.IsOwner( ply ) then
+            local result = http.base64Decode( text )
+            if (result) then
+                local data = string.split( result, ';' )
+                if (data[1] == chipName) then
+                    return data[2]
+                end
+            end
+
+            net.start( chipName )
+            net.writeString( text )
+            net.writeBool( isTeam )
+            net.send( ply )
+            return ''
+        end
     end)
 
 end
