d99f6987a1e9b678336e36f05a118c9a2fbc9bd7
PrikolMen <olegis019@gmail.com>
2023-01-24 17:23:29 +0400

    Simple Spawnpoint

---
+++ spawnpoint.lua
@@ -0,0 +1,46 @@
+--@name Spawnpoint
+--@author PrikolMen:-b
+--@includedir starfall_plib
+--@server
+
+--[[-----------------
+         Code
+-----------------]]--
+dofile( 'starfall_plib/init.lua' )
+local chipName = 'PLib - Spawnpoint'
+local posOffset = Vector( 0, 0, 25 )
+local angOffset = Angle( 90 )
+local plib = plib
+
+local chip = plib.Chip
+if (chip:getChipName() == 'Spawnpoint') then
+    chip:setCollisionGroup( 12 )
+    chip:setNoDraw( true )
+end
+
+do
+
+    local tr = plib.TraceLineUp( chip:getPos(), -100, chip )
+    if tr.Hit then
+        local ent = tr.Entity
+        if IsValid( ent ) and (ent:getOwner() == plib.Owner) then
+            chip:setMaterial( ent:getMaterial() )
+            chip:setColor( ent:getColor() )
+            chip:setParent( ent )
+        end
+    end
+
+end
+
+hook.Add('think', chipName, function()
+    local ok, ent = pcall( prop.create, chip:getPos(), chip:getAngles() - angOffset, 'models/props_trainstation/trainstation_clock001.mdl' )
+    if (ok) then
+        hook.Remove('think', chipName)
+        ent:setCollisionGroup( 12 )
+        ent:setParent( chip )
+
+        hook.Add('PlayerSpawn', chipName, function( ply )
+            plib.TeleportOwner( chip:localToWorld( posOffset ) )
+        end)
+    end
+end)
